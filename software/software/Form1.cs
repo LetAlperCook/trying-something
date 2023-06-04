b352d3dccd8942b8ab604f9b4f0ec5422def379f
LetAlperCook <135534509+LetAlperCook@users.noreply.github.com>
2023-06-04 20:29:03 +0300

    first update

---
+++ software/software/Form1.cs
@@ -0,0 +1,26 @@
+namespace software
+{
+    public partial class Form1 : Form
+    {
+        int result ,num1,num2 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "sum";
+            label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           num1=Convert.ToInt32(textBox1.Text);
+            num2=Convert.ToInt32(textBox2.Text);
+            result = num1+num2;
+            label1.Text = result.ToString();
+        }
+    }
+}
